c31c2ae56e91bd38520ca0f5e9efc7298efd6594
srce03 <juancarlosabucay@gmail.com>
2022-11-14 20:46:53 +0100

    Activitat Layout Juan Carlos

---
+++ app/src/main/java/com/example/layout/TableLayoutActivity.java
@@ -0,0 +1,12 @@
+package com.example.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TableLayoutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_table);
+    }
+}
